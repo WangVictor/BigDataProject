023fa98398789a6398031e1089ea69ebc76b0a26
ShengLiu <shengliu@nyu.edu>
2017-04-16 14:50:42 -0400

    solve indent porblems

--- col10.py
+++ col10.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-    sc = SparkContext()
-    lines = sc.textFile(sys.argv[1], 1) 
-    lines = lines.mapPartitions(lambda x: reader(x))
-    header = lines.first()
-    lines = lines.filter(lambda line: line != header)
-   	v10 =  lines.map(lambda x: x[10])
-    output10 = v10.map(lambda x : (str(x)+" TEXT indicator VALID") if len(x)>0 else ("NaN"+" TEXT indicator NULL"))
-    output10.saveAsTextFile("test10.out")
+	sc = SparkContext()
+	lines = sc.textFile(sys.argv[1], 1) 
+	lines = lines.mapPartitions(lambda x: reader(x))
+	header = lines.first()
+	lines = lines.filter(lambda line: line != header)
+	v10 =  lines.map(lambda x: x[10])
+	output10 = v10.map(lambda x : (str(x)+" TEXT indicator VALID") if len(x)>0 else ("NaN"+" TEXT indicator NULL"))
+	output10.saveAsTextFile("test10.out")
